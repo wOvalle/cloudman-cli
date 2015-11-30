81725845d1884264670804fc2ca3a43bddd980bb
Willy Ovalle <willyovalle16@gmail.com>
2015-11-30 01:05:48 -0400

    Adding cloudman.terminate

--- cloudman.js
+++ cloudman.js
@@ -7,6 +7,7 @@ program
     .command('status', 'print status of instances')
     .command('stop', 'stop instances')
     .command('start', 'start instances')
+    .command('terminate', 'delete instances')
     .parse(process.argv);
 
 if (!program.args.length) program.help();
