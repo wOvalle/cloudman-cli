4c595060352ccc3bf02fef0cbca76ab2288ab817
Willy Ovalle <willyovalle16@gmail.com>
2015-11-30 00:55:54 -0400

    Adding cloudman.stop

--- cloudman.js
+++ cloudman.js
@@ -4,5 +4,8 @@ var program = require('commander');
 
 program
     .version('0.0.1')
-    .command('status', 'print status of instances', {isDefault: true})
+    .command('status', 'print status of instances')
+    .command('stop', 'stop instances')
     .parse(process.argv);
+
+if (!program.args.length) program.help();
